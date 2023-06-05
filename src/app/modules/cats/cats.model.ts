3ceb5e1f8d66979a969c783806fd4261667ecfed
Zihad550 <dev.jehadhossain@gmail.com>
2023-06-05 17:37:47 +0600

    feat: add documentation

--- src/app/modules/cats/cats.model.ts
+++ src/app/modules/cats/cats.model.ts
@@ -1,6 +1,39 @@
 import { Schema, model } from 'mongoose';
 import ICat from './cats.interface';
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     CreateCatInput:
+ *       type: object
+ *       required:
+ *         - name
+ *         - nicName
+ *         - ownerId
+ *       properties:
+ *         name:
+ *           type: string
+ *         nicname:
+ *           type: string
+ *         ownerId:
+ *           type: string
+ *     CreateCatResponse:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *         name:
+ *           type: string
+ *         nicname:
+ *           type: string
+ *         ownerId:
+ *           type: string
+ *         createdAt:
+ *           type: string
+ *         updatedAt:
+ *           type: string
+ */
 const catSchema = new Schema<ICat>({
   name: {
     type: String,
--- src/app/modules/cats/cats.route.ts
+++ src/app/modules/cats/cats.route.ts
@@ -3,6 +3,31 @@ import catsController from './cats.controller';
 
 const router = Router();
 
+/**
+ * @openapi
+ * /api/v1/cats/create-cat:
+ *   post:
+ *    tags:
+ *      - Cat
+ *    summary: Create cat
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/CreateCatInput'
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/CreateCatResponse'
+ *      409:
+ *         description: Conflict
+ *      400:
+ *         description: Bad request
+ */
 router.post('/create-cat', catsController.createCat);
 router.get('/all', catsController.getAllCat);
 router.get('/owner/:id', catsController.getCatsByOwnerId);
